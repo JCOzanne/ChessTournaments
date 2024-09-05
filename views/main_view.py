5966df13aa8dcbecc00fca959bc90e1d0d99e369
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2024-09-05 07:50:09 +0200

    mise à jour des vues pour gérer l'arrêt du programme

--- views/main_view.py
+++ views/main_view.py
@@ -1,11 +1,12 @@
 def display_main_menu():
     print("1. Créer un tournoi")
     print("2. Générer un rapport")
-    print("3. Quitter")
+    print("3. Revenir où je me suis arrêté")
+    print("4. Quitter")
 
-    while True:  # Boucle jusqu'à ce que l'utilisateur entre une valeur correcte
+    while True:
         choice = input("Choisissez une option: ")
-        if choice.isdigit() and int(choice) in [1, 2, 3]:
+        if choice.isdigit() and int(choice) in [1, 2, 3, 4]:
             return choice
         else:
             print("Entrée invalide. Veuillez entrer un chiffre correspondant à une option.")
